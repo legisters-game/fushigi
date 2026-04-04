8a0b173cafc3eef0ee4d3f7e98149d67a04d4b20
レジスタス <takumi060827@gmail.com>
2026-04-04 22:50:48 +0900

    可視範囲設定完了

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+@export var 飛行:bool
+@export var キー:InputEventKey
+const SPEED = 7
 const JUMP_VELOCITY = 3
 const 感度:float=5
 var 視点回転:Vector2
@@ -11,7 +12,7 @@ func _ready() -> void:
 	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor()and not 飛行:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
@@ -22,19 +23,38 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var 速度:float=13
+	if Input.is_action_pressed("アップ"):
+		direction.y=速度*delta
+	elif Input.is_action_pressed("ダウン"):
+		direction.y=-速度*delta
+	else:
+		direction.y=0
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		velocity.y = direction.y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
 	rotation_degrees.y-=視点回転.x*delta*感度
 	視点回転=Vector2.ZERO
+	
+	
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var マウス:InputEventMouseMotion=event
 		視点回転=マウス.screen_relative
+	elif event is InputEventKey:
+		var イベント:InputEventKey=event
+		if イベント.keycode==4194305 and イベント.pressed:
+			マウスチェンジ()
 	
+func マウスチェンジ()->void:
+	if Input.mouse_mode==Input.MOUSE_MODE_VISIBLE:
+		Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
+	else:
+		Input.mouse_mode=Input.MOUSE_MODE_VISIBLE
--- 街チャンク処理/チャンクルート.gd
+++ 街チャンク処理/チャンクルート.gd
@@ -4,21 +4,21 @@ extends Node3D
 var 保存パス:String
 func _ready() -> void:
 	if Engine.is_editor_hint():
-		for i in get_children():
-			if i is Area3D:
-				
-				scale=Vector3(-3,-3,-3)
-				var ベース位置:Vector3=i.global_position
-				scale=Vector3(1,1,1)
-				var ベース:Vector3=i.scale
+		#for i in get_children():
+			#if i is MeshInstance3D:
+				#i.show()
+				#scale=Vector3(-3,-3,-3)
+				#var ベース位置:Vector3=i.global_position
+				#scale=Vector3(1,1,1)
+				#var ベース:Vector3=i.scale
 				#i.scale.x=i.scale.x+16
 				#i.scale.y=i.scale.y+16
 				#i.scale.z=i.scale.z+16
-				i.global_position=ベース位置
+				#i.global_position=ベース位置
 				
 			
 		return
-	show()
+	hide()
 	処理有無制御(false)
 	デバッグ(true)
 	保存パス = name.replace("_Chunk", "")+".tscn"
@@ -34,6 +34,7 @@ func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		処理有無制御(false)
 		デバッグ(true)
+		hide()
 
 
 
